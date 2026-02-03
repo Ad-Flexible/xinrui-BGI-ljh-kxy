796e45dad93a8c22d3969aaa46650846d8220b79
kexinyi <fearless614@qq.com>
2026-02-03 14:57:53 +0800

    dto补充

--- src/main/java/org/xinrui/dto/PatientDTO.java
+++ src/main/java/org/xinrui/dto/PatientDTO.java
@@ -1,7 +1,17 @@
 package org.xinrui.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
 @Data
 public class PatientDTO {
 
-    @ApiModelProperty(value = "")
+    @ApiModelProperty(value = "产品套餐编号")
+    private String productNo;
+
+    @ApiModelProperty(value="产品套餐名称")
+    private String productName;
+
+    @ApiModelProperty(value="样本编号")
+    private String sampleId;
 }
